d692825a82893cda0ab0111bbddbea02958e6187
Dhananjay Sahoo <kanha94377@gmail.com>
2024-06-09 07:49:38 +0530

    stopped scrolling when loading aniimation is in effect

--- Scripts/loader.js
+++ Scripts/loader.js
@@ -1,5 +1,11 @@
+
+    document.body.classList.
+    add("stop-scrolling");
+
+
 $(window).on("load",function(){
     //$(".wrapper-loader").fadeOut("slow")
     // document.querySelectorAll(".wrapper-loader").style.display = "none"
     $(".loader-wrapper").css("display","none");
+        document.body.classList.remove("stop-scrolling");
 });
